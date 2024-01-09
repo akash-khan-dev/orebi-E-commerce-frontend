8e76c59ae4b17a8c0b1bece5b7e117233b6d8854
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-01-09 15:07:13 +0600

    banner design complete

---
+++ src/components/pageComponent/Home/Banner.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import bannerImg from "../../../assets/Images/banner.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+const Banner = () => {
+  const [doteActive, setDoteActive] = useState(0);
+  const settings = {
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    mouseHover: false,
+    beforeChange: (prev, next) => {
+      setDoteActive(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          position: "absolute",
+          top: "34%",
+          left: "-40%",
+        }}
+      >
+        <li style={{ display: "inline-block" }}>{dots} </li>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={
+          i === doteActive
+            ? {
+                width: "30px",
+                color: "#262626",
+                borderRight: "3px #262626 solid",
+              }
+            : {
+                width: "30px",
+                color: "transparent",
+                borderRight: "3px white solid",
+                color: "transparent",
+              }
+        }
+      >
+        0{i + 1}
+      </div>
+    ),
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          dots: true,
+          appendDots: (dots) => (
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "-48%",
+                transform: "translateY(-50%)",
+              }}
+            >
+              <li style={{ display: "inline-block" }}>{dots} </li>
+            </div>
+          ),
+          customPaging: (i) => (
+            <div
+              style={
+                i === doteActive
+                  ? {
+                      width: "30px",
+                      color: "#262626",
+                      borderRight: "3px #262626 solid",
+                      fontSize: "12px",
+                    }
+                  : {
+                      width: "30px",
+                      color: "transparent",
+                      borderRight: "3px white solid",
+                      color: "transparent",
+                      fontSize: "12px",
+                    }
+              }
+            >
+              0{i + 1}
+            </div>
+          ),
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <section className="Banner">
+        <div>
+          <Slider {...settings}>
+            <div>
+              <a href="/">
+                <img src={bannerImg} alt="banner" />
+              </a>
+            </div>
+            <div>
+              <a href="/">
+                <img src={bannerImg} alt="banner" />
+              </a>
+            </div>
+            <div>
+              <a href="/">
+                <img src={bannerImg} alt="banner" />
+              </a>
+            </div>
+          </Slider>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../../components/pageComponent/Home/Banner";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+};
+
+export default Home;
