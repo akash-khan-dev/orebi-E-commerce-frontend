1a70da1394ecc5a276d9de41f328c0837952f6d3
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-01-09 15:13:31 +0600

    additional information design complete

---
+++ src/components/pageComponent/Home/AdditionalInfo.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BiSolidTruck } from "react-icons/bi";
+import { TbReload } from "react-icons/tb";
+const AdditionalInfo = () => {
+  return (
+    <>
+      <section className="additional-info">
+        <div className="container">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-1 sm:gap-2 items-center">
+              <h3 className="text-black text-xl sm:text-4xl font-bold">2</h3>
+              <p className="font-primaryFont font-normal text-xs sm:text-base text-[#6D6D6D]">
+                Two years warranty
+              </p>
+            </div>
+            <div className="flex gap-1 sm:gap-2 items-center">
+              <div className="text-black text-xl sm:text-4xl font-bold ">
+                <BiSolidTruck />
+              </div>
+              <h4 className="font-primaryFont font-normal first-letter text-xs sm:text-base text-[#6D6D6D]">
+                Free shipping
+              </h4>
+            </div>
+            <div className="flex gap-2 items-center">
+              <div className="text-black text-xl sm:text-4xl font-bold ">
+                <TbReload />
+              </div>
+              <h4 className="font-primaryFont font-normal text-xs sm:text-base text-[#6D6D6D]">
+                Free shipping
+              </h4>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default AdditionalInfo;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../components/pageComponent/Home/Banner";
+import AdditionalInfo from "../../components/pageComponent/Home/AdditionalInfo";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <AdditionalInfo />
     </>
   );
 };
