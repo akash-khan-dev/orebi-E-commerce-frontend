4d6230cb3ca94dd8208a80c7fce9467b882142b9
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-01-24 22:02:47 +0600

    cart button redesign

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ text }) => {
     <div className="button mt-7">
       <button
         type="button"
-        className="font-primaryFont font-bold text-sm w-44 h-10  text-[#2b2b2b] overflow-hidden shadow-lg border-[#2b2b2b] before:block before:absolute before:translate-x-[90%] before:inset-0 before:bg-[#2b2b2b] before:hover:translate-x-0 before:duration-300 before:rounded-s-full before:-z-10 after:-z-10 after:rounded-e-full after:duration-300 after:hover:translate-x-0 after:block after:absolute after:-translate-x-[90%] after:inset-0  after:bg-[#2b2b2b] relative inline-block hover:text-white"
+        className="font-primaryFont font-bold text-sm w-44 h-11  text-[#2b2b2b] overflow-hidden shadow-lg border-[#2b2b2b] before:block before:absolute before:translate-x-[90%] before:inset-0 before:bg-[#2b2b2b] before:hover:translate-x-0 before:duration-300 before:rounded-s-full before:-z-10 after:-z-10 after:rounded-e-full after:duration-300 after:hover:translate-x-0 after:block after:absolute after:-translate-x-[90%] after:inset-0  after:bg-[#2b2b2b] relative inline-block hover:text-white"
       >
         {text}
       </button>
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -158,14 +158,7 @@ const Cart = () => {
                     </span>
                   </div>
                 </div>
-                <div className="button mt-7">
-                  <button
-                    type="button"
-                    className="font-primaryFont font-bold text-sm w-44 h-10  text-[#2b2b2b] overflow-hidden shadow-lg border-[#2b2b2b] before:block before:absolute before:translate-x-[90%] before:inset-0 before:bg-[#2b2b2b] before:hover:translate-x-0 before:duration-300 before:rounded-s-full before:-z-10 after:-z-10 after:rounded-e-full after:duration-300 after:hover:translate-x-0 after:block after:absolute after:-translate-x-[90%] after:inset-0  after:bg-[#2b2b2b] relative inline-block hover:text-white"
-                  >
-                    Proceed to Checkout
-                  </button>
-                </div>
+                <Button text={"Proceed to Checkout"} />
               </div>
             </div>
           </div>
