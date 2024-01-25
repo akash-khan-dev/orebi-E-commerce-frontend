4aa7268efa4cba734673ed2a61da6544adbc5aa6
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-01-25 22:08:25 +0600

    login form validation with formik

--- src/components/pageComponent/login/LoginForm.jsx
+++ src/components/pageComponent/login/LoginForm.jsx
@@ -3,8 +3,22 @@ import FormHeading from "../../common/FormHeading";
 import Input from "../../common/Input";
 import { IoEyeOffOutline, IoEye } from "react-icons/io5";
 import Button from "../../common/Button";
+import InputError from "../../common/InputError";
+import { signIn } from "../../../validation/validation";
+import { useFormik } from "formik";
 const LoginForm = () => {
   const [showPass, setShowPass] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: signIn,
+    onSubmit: (data) => {
+      console.log(data);
+    },
+  });
   return (
     <>
       <div>
@@ -12,30 +26,41 @@ const LoginForm = () => {
           <FormHeading title="Returning Customer" />
         </div>
         <div className="form">
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <div className=" sm:grid sm:grid-cols-2 xl:grid-cols-3 sm:gap-6 md:gap-10">
-              <Input
-                type="email"
-                name="email"
-                onChange=""
-                value=""
-                placeholder="company@domain.com"
-                label="Email Address"
-                id="email"
-              />
+              <div>
+                <Input
+                  type="email"
+                  name="email"
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                  placeholder="company@domain.com"
+                  label="Email Address"
+                  id="email"
+                />
+                <InputError
+                  error={formik.errors.email}
+                  touched={formik.touched.email}
+                />
+              </div>
               <div className="relative">
                 <Input
                   type={showPass ? "text" : "password"}
                   name="password"
                   onChange=""
-                  // value=""
+                  value={formik.values.password}
                   placeholder="password"
                   label="Password"
                   id="password"
                 />
+                <InputError
+                  error={formik.errors.password}
+                  touched={formik.touched.password}
+                />
+
                 <div
                   onClick={() => setShowPass(!showPass)}
-                  className="absolute top-2/4 -translate-y-1/4 right-5 md:right-7 lg:right-8 cursor-pointer"
+                  className="absolute top-[60%]  right-5 md:right-7 lg:right-8 cursor-pointer"
                 >
                   {showPass ? <IoEye /> : <IoEyeOffOutline />}
                 </div>
